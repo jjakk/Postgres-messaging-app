6ede73d764ddc4dffb55f28dd8377438ce3f23a8
John Khachian <jaksawesome10@gmail.com>
2022-05-28 12:26:55 -0400

    hopeful database fix

--- db.js
+++ db.js
@@ -1,4 +1,8 @@
 const { Pool } = require('pg')
-const pool = new Pool()
+const pool = new Pool(process.env.NODE_ENV ?
+    {
+        connectionString: proccess.env.DATABASE_URL
+    } : null
+);
 
 module.exports = pool;
